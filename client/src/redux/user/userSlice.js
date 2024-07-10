5d216fb21a8b2c15ce4e6a2557c5c9e7fa8b46df
Anurag Shukla <shuklaanurag310@gmail.com>
2024-07-10 18:38:26 +0530

    .

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,78 +1,7 @@
-// import { createSlice } from '@reduxjs/toolkit';
-
-
-// // const storedData = localStorage.getItem("data");
-// // let parsedData = null;
-
-// // try {
-// //     if (storedData && typeof storedData === 'string' && storedData.trim() !== '') {
-// //         // eslint-disable-next-line no-unused-vars
-// //         parsedData = JSON.parse(storedData);
-// //     }
-// // } catch (error) {
-// //     console.error("Error parsing stored data:", error);
-// // }
-
-// // const initialState = {
-// //     data: null,
-// //     currentUser: null
-// // };
-
-
-
-// // const tokenLocalStorage = localStorage.getItem("token");
-// // console.log(tokenLocalStorage)
-
-// const initialState = {
-//     // token : localStorage.getItem("token") || null,
-//     isAuthenticated: false,
-//     currentUser: null
-// };
-
-// const userSlice = createSlice({
-//     name: 'user',
-//     initialState,
-
-//     reducers: {
-//         signInSuccess: (state, action) => {
-//             state.isAuthenticated = true;
-//             state.currentUser = action.payload;
-//             console.log(state.currentUser);
-//         },
-//     }
-// });
-
-
-
-// export const { signInSuccess } = userSlice.actions;
-
-// export default userSlice.reducer;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    // localstorage: localStorage.getItem("userdata") || null,
     token: localStorage.getItem('token'),
     isLoggedIn: false,
     userData: null,
@@ -85,15 +14,11 @@ const userSlice = createSlice({
         login(state, action) {
             state.userData = action.payload;
             state.isLoggedIn = true;
-            // localStorage.setItem('userdata', JSON.stringify(action.payload));
-            // console.log(action.payload)
             state.token = action.payload.token;
             localStorage.setItem('token', action.payload.token);
         },
         logout(state) {
             state.isLoggedIn = false;
-            // state.userData = null;
-            // localStorage.removeItem('userdata');
             state.token = null;
             localStorage.removeItem('token');
         },
