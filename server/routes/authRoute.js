7adea1cbfee16cf7f51e56e2bf986c25e2ddb321
Anurag Shukla <shuklaanurag310@gmail.com>
2024-07-16 11:56:28 +0530

    .

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { logout, signin, signup } from '../controllers/authController.js';
+import { google, logout, signin, signup } from '../controllers/authController.js';
 
 
 
@@ -9,5 +9,8 @@ const router = express.Router();
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.get('/logout', logout);
+
+
+router.post('/google', google);
 
 export default router;
