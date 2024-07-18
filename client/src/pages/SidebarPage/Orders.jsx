6cee6dc85a89025301210aaece63b9216bf57bcc
Anurag Shukla <shuklaanurag310@gmail.com>
2024-07-18 16:59:14 +0530

    .

--- client/src/pages/SidebarPage/Orders.jsx
+++ client/src/pages/SidebarPage/Orders.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React, { useEffect, useState } from 'react';
@@ -5,9 +6,13 @@ import { AddDetailsModal } from '../../component/AddNewProduct';
 import { Link } from 'react-router-dom';
 
 
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+
 export const Orders = () => {
     const [productDetails, setProductDetails] = useState([]);
     const [loading, setLoading] = useState(true);
+
 
     //// this is for AddDetails
     const [isModalOpen, setIsModalOpen] = useState(false);
@@ -30,7 +35,7 @@ export const Orders = () => {
                     throw new Error('Network response was not ok');
                 }
                 const data = await res.json();
-                console.log(data);
+                console.log(data.data);
                 if (data.success && data.data.length > 0) {
                     setProductDetails(data.data);
                 } else {
@@ -49,56 +54,70 @@ export const Orders = () => {
 
 
     return (
-        <div className="container mx-auto px-4 py-8">
-            <h1 className="text-2xl font-bold mb-4">Orders</h1>
-            <button className="bg-green-800 m-10 p-2 w-28 text-white rounded-lg text-xl" onClick={openModal}>Add</button>
-            {
-                loading ? "Loading..........." : (
-                    <div className="overflow-x-auto">
-                        <table className="table-auto border-collapse border border-gray-200 w-full">
-                            <thead>
-                                <tr className="bg-blue-400">
-                                    <th className="border border-gray-800 px-4 py-2">Order id</th>
-                                    <th className="border border-gray-800 px-4 py-2">Product Name</th>
-                                    <th className="border border-gray-800 px-4 py-2">Order Value</th>
-                                    <th className="border border-gray-800 px-4 py-2">Customer Details</th>
-                                    <th className="border border-gray-800 px-4 py-2">Billable Weight</th>
-                                    <th className="border border-gray-800 px-4 py-2">Pincode</th>
-                                    <th className="border border-gray-800 px-4 py-2">State</th>
-                                    <th className="border border-gray-800 px-4 py-2">Shipping Details</th>
-                                    <th className="border border-gray-800 px-4 py-2">Famous Landmark</th>
-                                    <th className="border border-gray-800 px-4 py-2">Order Date</th>
-                                    <th className="border border-gray-800 px-4 py-2">Payment Mode</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                {productDetails.map((detail) => (
-                                    <tr key={detail._id} className="bg-slate-200">
-                                        <td className="border border-gray-800 px-4 py-2">{detail.orderId}</td>
-                                        <td className="border border-gray-800 px-4 py-2">{detail.productName}</td>
-                                        <td className="border border-gray-800 px-4 py-2 flex flex-col"><div>₹{detail.orderValue}</div> <div>{detail.paymentMode}</div></td>
-                                        <td className="border border-gray-800 px-4 py-2" > <Link to={`/ordersbyid/${detail._id}`}><div>{detail.fullName}</div></Link>   <div>{detail.mobileNo}</div></td>
-                                        <td className="border border-gray-800 px-4 py-2">{detail.physicalWeight}</td>
-                                        <td className="border border-gray-800 px-4 py-2">{detail.pincode}</td>
-                                        <td className="border border-gray-800 px-4 py-2">{detail.state}</td>
-                                        <td className="border border-gray-800 px-4 py-2">{detail.pickupLocation}</td>
-                                        <td className="border border-gray-800 px-4 py-2">{detail.landmark}</td>
-                                        <td className="border border-gray-800 px-4 py-2">{new Date(detail.orderDate).toLocaleDateString()}</td>
-                                        <td className="border border-gray-800 px-4 py-2">{detail.paymentMode}</td>
+        <>
+            <div className="container mx-auto px-4 py-8">
+                <h1 className="text-2xl font-bold mb-4">Orders</h1>
+                <button className="bg-green-800 m-10 p-2 w-28 text-white rounded-lg text-xl" onClick={openModal}>Add</button>
+                {
+                    loading ? "Loading..........." : (
+                        <div className="overflow-x-auto">
+                            <table className="table-auto border-collapse border border-gray-200 w-full">
+                                <thead>
+                                    <tr className="bg-blue-400">
+                                        <th className="border border-gray-800 px-4 py-2">Order id</th>
+                                        <th className="border border-gray-800 px-4 py-2">Product Name</th>
+                                        <th className="border border-gray-800 px-4 py-2">Order Value</th>
+                                        <th className="border border-gray-800 px-4 py-2">Customer Details</th>
+                                        <th className="border border-gray-800 px-4 py-2">Billable Weight</th>
+                                        <th className="border border-gray-800 px-4 py-2">Pincode</th>
+                                        <th className="border border-gray-800 px-4 py-2">State</th>
+                                        <th className="border border-gray-800 px-4 py-2">Shipping Details</th>
+                                        <th className="border border-gray-800 px-4 py-2">Famous Landmark</th>
+                                        <th className="border border-gray-800 px-4 py-2">Order Date</th>
+                                        <th className="border border-gray-800 px-4 py-2">Payment Mode</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                </thead>
 
-                        <AddDetailsModal
-                            open={isModalOpen}
-                            close={closeModal}
-                        />
-                    </div>
-                )
-            }
-        </div>
+                                <tbody>
+                                    {productDetails.map((detail) => (
+                                        <tr key={detail._id} className="bg-slate-200">
+                                            <td className="border border-gray-800 px-4 py-2">{detail.orderId}</td>
+                                            <td className="border border-gray-800 px-4 py-2">{detail.productName}</td>
+                                            <td className="border border-gray-800 px-4 py-2 flex flex-col"><div>₹{detail.orderValue}</div> <div>{detail.paymentMode}</div></td>
+                                            <td className="border border-gray-800 px-4 py-2" > <Link to={`/ordersbyid/${detail._id}`}><div>{detail.fullName}</div></Link>   <div>{detail.mobileNo}</div></td>
+                                            <td className="border border-gray-800 px-4 py-2">{detail.physicalWeight}</td>
+                                            <td className="border border-gray-800 px-4 py-2">{detail.pincode}</td>
+                                            <td className="border border-gray-800 px-4 py-2">{detail.state}</td>
+                                            <td className="border border-gray-800 px-4 py-2">{detail.pickupLocation}</td>
+                                            <td className="border border-gray-800 px-4 py-2">{detail.landmark}</td>
+                                            <td className="border border-gray-800 px-4 py-2">{new Date(detail.orderDate).toLocaleDateString()}</td>
+                                            <td className="border border-gray-800 px-4 py-2">{detail.paymentMode}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+
+                            <AddDetailsModal
+                                open={isModalOpen}
+                                close={closeModal}
+                            />
+                        </div>
+                    )
+                }
+            </div>
+
+    
+
+            <div className="card">
+                <DataTable value={productDetails} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]} tableStyle={{ minWidth: '50rem' }}>
+                    <Column field="orderId" header="Order ID" style={{ width: '25%' }}></Column>
+                    <Column field="productName" header="Product Name" style={{ width: '25%' }}></Column>
+                    <Column field="orderValue" header="Order Value" style={{ width: '25%' }}></Column>
+                    <Column field="fullName" header="Customer Details" style={{ width: '25%' }}></Column>
+                    <Column field="physicalWeight" header="Billable Weight" style={{ width: '25%' }}></Column>
+                </DataTable>
+            </div>
+        </>
     );
 };
 
