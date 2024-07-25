0365fcc025f95be5fb34d3371aac0244d5856285
Anurag Shukla <shuklaanurag310@gmail.com>
2024-07-25 15:29:39 +0530

    .

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -25,36 +25,3 @@ root.render(
 );
 
 reportWebVitals();
-
-
-
-
-
-// import React from 'react';
-// import { createRoot } from 'react-dom/client';
-// import { AuthContextProvider, ConfigProvider } from './contexts/ConfigContext';
-// import { PrimeReactProvider } from 'primereact/api';
-// import 'primereact/resources/themes/lara-light-cyan/theme.css';
-// import 'primeicons/primeicons.css';
-
-// import './index.scss';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import { Toaster } from 'react-hot-toast';
-
-
-// const container = document.getElementById('root');
-// const root = createRoot(container);
-
-// function renderApp() {
-//   root.render(
-//     <ConfigProvider>
-//       <AuthContextProvider>
-//         <PrimeReactProvider>
-//           <Toaster />
-//           <App />
-//         </PrimeReactProvider>
-//       </AuthContextProvider>
-//     </ConfigProvider>
-//   );
-// }
