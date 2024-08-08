d223318d3378407069b10b652d45aea10b34f142
Anurag Shukla <shuklaanurag310@gmail.com>
2024-08-08 16:39:54 +0530

    .

--- frontend/src/views/auth/signup/SignUp1.jsx
+++ frontend/src/views/auth/signup/SignUp1.jsx
@@ -1,10 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Row, Col } from 'react-bootstrap';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink, Link, useNavigate } from 'react-router-dom';
 
 import Breadcrumb from '../../../layouts/AdminLayout/Breadcrumb';
 
 const SignUp1 = () => {
+
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    touched: false
+  });
+
+  const [loading, setLoading] = useState(false);
+
+
+  const handleBlur = () => {
+    if (userData.email.trim() === '' || userData.password.trim() === '') {
+      setUserData({
+        ...userData,
+        touched: true
+      });
+    }
+  };
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+    console.log(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true)
+    try {
+      const res = await fetch('https://officeproject.onrender.com/api/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(userData),
+      });
+
+      const resData = await res.json();
+      console.log(resData);
+
+      // const resData = await res
+      setUser(res);
+      if (res.ok) {
+        setLoading(false);
+        navigate('/signin');
+      }
+    } catch (error) {
+      console.error('Signin error:', error);
+    }
+  };
+
+
   return (
     <React.Fragment>
       <Breadcrumb />
@@ -25,21 +77,21 @@ const SignUp1 = () => {
                   </div>
                   <h3 className="mb-4">Sign up</h3>
                   <div className="input-group mb-3">
-                    <input type="text" className="form-control" placeholder="Username" />
+                    <input type="text" name='name' onChange={handleChange} className="form-control" placeholder="Username" />
                   </div>
                   <div className="input-group mb-3">
-                    <input type="email" className="form-control" placeholder="Email address" />
+                    <input type="email" name='email' onChange={handleChange} className="form-control" placeholder="Email address" />
                   </div>
                   <div className="input-group mb-4">
-                    <input type="password" className="form-control" placeholder="Password" />
+                    <input type="password" name='password' onChange={handleChange} className="form-control" placeholder="Password" />
                   </div>
-                  <div className="form-check  text-start mb-4 mt-2">
+                  {/* <div className="form-check  text-start mb-4 mt-2">
                     <input type="checkbox" className="form-check-input" id="customCheck1" defaultChecked={false} />
                     <label className="form-check-label" htmlFor="customCheck1">
                       Send me the <Link to="#"> Newsletter</Link> weekly.
                     </label>
-                  </div>
-                  <button className="btn btn-primary mb-4">Sign up</button>
+                  </div> */}
+                  <button className="btn btn-primary mb-4" onClick={handleSubmit}>Sign up</button>
                   <p className="mb-2">
                     Already have an account?{' '}
                     <NavLink to={'/auth/signin-1'} className="f-w-400">
